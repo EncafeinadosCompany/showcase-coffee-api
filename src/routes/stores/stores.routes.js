7e33d3f9a43b4c0ac40c3f117fb7c8ba28365b4e
Santiago Florez <santiagoflorezv23@gmail.com>
2025-01-29 17:30:31 -0500

    refactor: simplify store routes by removing redundant path prefix

--- src/routes/stores/stores.routes.js
+++ src/routes/stores/stores.routes.js
@@ -4,9 +4,9 @@ const { StoreController } = require('../../controllers/store/store.controller');
 const router = express.Router();
 const storeController = new StoreController();
 
-router.post('/stores', (req, res) => storeController.createStore(req, res));
-router.get('/stores', (req, res) => storeController.getAllStores(req, res));
-router.get('/stores/:id', (req, res) => storeController.getStoreById(req, res));
-router.put('/stores/:id', (req, res) => storeController.updateStore(req, res));
+router.post('/', (req, res) => storeController.createStore(req, res));
+router.get('/', (req, res) => storeController.getAllStores(req, res));
+router.get('/:id', (req, res) => storeController.getStoreById(req, res));
+router.put('/:id', (req, res) => storeController.updateStore(req, res));
 
 module.exports = router ;
