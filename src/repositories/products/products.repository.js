913f94624492a302478696d97be31b20c6c81d08
valentina-RGB <polar.loc102@gmail.com>
2025-01-29 14:30:46 -0500

    perf: :construction: drop domain

--- src/repositories/products/products.repository.js
+++ src/repositories/products/products.repository.js
@@ -1,5 +1,30 @@
 
-const Product = require('../../domain/products/products.entity');
-const {} = require('../../models/products/products.entity')
 
+const {ProductModel} = require('../../models/products/products.entity')
+const {BrandModel} = require('../../models/brands/brands.entity')
 console.log(Product)
+
+class ProductsRepository {
+
+    constructor() {}
+    async getAll() {
+
+    const products = await ProductModel.findAll()
+    return products
+    }
+
+    async getById(id) {
+        const product = await ProductModel.findByPk(id , {
+            include: [{
+                model:BrandModel,
+                as: 'brands'
+            }]
+        })
+        return !product ? null : product    
+    }
+
+    async create(product){
+        const newProduct = await ProductModel.create(product)
+        return newProduct
+    }
+}
