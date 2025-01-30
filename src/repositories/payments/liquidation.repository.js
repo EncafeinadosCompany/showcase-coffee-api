166dad28594fdf70a9ee243f7d83dec3e4e0e61d
Jose David Henao Camacho <jose_dhenaoc@soy.sena.edu.co>
2025-01-30 11:52:04 -0500

    create -deposit.repository -liquidation.repository -deposit.service -liquidation.service

--- src/repositories/payments/liquidation.repository.js
+++ src/repositories/payments/liquidation.repository.js
@@ -1,23 +1,54 @@
-const {LiquidationModel} = require('../../models');
+const { LiquidationModel } = require('../../models/payments/liquidation.entity');
+const { DepositModel } = require('../../models/payments/deposit.entity');
 
 class LiquidationRepository {
-    
-    constructor() {}
+  constructor() {}
 
-    async getLiquidations() {
-        return await LiquidationModel.findAll();
+  async getLiquidations() {
+    try {
+      return await LiquidationModel.findAll();
+    } catch (error) {
+      console.error('Error al obtener las liquidaciones:', error);
+      throw new Error('No se pudo obtener la lista de liquidaciones.');
     }
+  }
 
-    async getLiquidationById(id) {
-        return await LiquidationModel.findByPk(id);
+  async getLiquidationById(id) {
+    try {
+      return await LiquidationModel.findByPk(id);
+    } catch (error) {
+      console.error(`Error al obtener la liquidación con ID ${id}:`, error);
+      throw new Error('No se pudo obtener la liquidación.');
     }
+  }
 
-    async createLiquidation(liquidation) {
-        const newLiquidation = await LiquidationModel.create(liquidation);
-        return newLiquidation;
+  async createLiquidation(liquidation) {
+    try {
+      return await LiquidationModel.create(liquidation);
+    } catch (error) {
+      console.error('Error al crear la liquidación:', error);
+      throw new Error('No se pudo crear la liquidación.');
     }
+  }
 
+  async getLiquidationWithDeposits(id) {
+    try {
+      return await LiquidationModel.findByPk(id, {
+        include: [
+          {
+            model: DepositModel,
+            as: 'deposits',
+          },
+        ],
+      });
+    } catch (error) {
+      console.error(`Error al obtener la liquidación con depósitos (ID ${id}):`, error);
+      throw new Error('No se pudo obtener la liquidación con depósitos.');
+    }
+  }
 }
 
-module.exports = LiquidationRepository
+module.exports = LiquidationRepository;
+
+
 
