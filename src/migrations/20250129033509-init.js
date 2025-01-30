70793186d16649aedc5f31dd85c426adec99eb13
Santiago Florez <santiagoflorezv23@gmail.com>
2025-01-30 15:38:03 -0500

    feat: add image variant support to product variants

--- src/migrations/20250129033509-init.js
+++ src/migrations/20250129033509-init.js
@@ -12,6 +12,7 @@ const { BRAND_TABLE, brandSchema } = require("../models/products/brands.entity")
 const { PRODUCT_TABLE, productSchema } = require("../models/products/products.entity");
 const { ATTRIBUTE_TABLE, attributeSchema } = require("../models/products/attribute.entity");
 const { VARIANT_PRODUCT_TABLE, variantProductSchema } = require("../models/products/variantsProducts.entity");
+const { IMAGE_VARIANTS_TABLE, imageVariantSchema } = require("../models/products/image_variants.entity");
 const { PRODUCT_PROVIDER_TABLE, productProviderSchema } = require("../models/providers/products_providers.entity");
 const { ATTRIBUTE_PRODUCT_TABLE, attributeProductSchema } = require("../models/products/attributesProducts.entity");
 
@@ -35,6 +36,7 @@ module.exports = {
     await queryInterface.createTable(PRODUCT_TABLE, productSchema);
     await queryInterface.createTable(ATTRIBUTE_TABLE, attributeSchema);
     await queryInterface.createTable(VARIANT_PRODUCT_TABLE, variantProductSchema);
+    await queryInterface.createTable(IMAGE_VARIANTS_TABLE, imageVariantSchema);
     await queryInterface.createTable(PRODUCT_PROVIDER_TABLE, productProviderSchema);
     await queryInterface.createTable(ATTRIBUTE_PRODUCT_TABLE, attributeProductSchema);
     
@@ -58,6 +60,7 @@ module.exports = {
     await queryInterface.dropTable(BRAND_TABLE);
     await queryInterface.dropTable(ATTRIBUTE_TABLE);
     await queryInterface.dropTable(VARIANT_PRODUCT_TABLE);
+    await queryInterface.dropTable(IMAGE_VARIANTS_TABLE);
     await queryInterface.dropTable(PRODUCT_PROVIDER_TABLE);
 
     await queryInterface.dropTable(SALE_TABLE);
