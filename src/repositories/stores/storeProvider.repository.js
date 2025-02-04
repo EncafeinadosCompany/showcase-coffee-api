6c7a7bb2e4c7a4864d459456cc78ae5df07432d4
Santiago Florez <santiagoflorezv23@gmail.com>
2025-02-04 17:51:53 -0500

    refactor: :zap: Update field names in StoreProvider repository for consistency

--- src/repositories/stores/storeProvider.repository.js
+++ src/repositories/stores/storeProvider.repository.js
@@ -3,20 +3,21 @@ const { StoreProviderModel } = require("../../models/stores/storeProvider.entity
 class StoreProviderRepository {
   async addStoreProvider(storeId, providerId) {
     return await StoreProviderModel.create({
-      store_id: storeId,
-      provider_id: providerId,
+      id_store: storeId, 
+      id_provider: providerId, 
     });
   }
 
+
   async getProvidersByStore(storeId) {
     return await StoreProviderModel.findAll({
-      where: { store_id: storeId },
+      where: { id_store: storeId }, 
       include: ["provider"], 
     });
   }
   async getStoresByProvider(providerId) {
     return await StoreProviderModel.findAll({
-      where: { provider_id: providerId },
+      where: { id_provider: providerId },
       include: ["store"], 
     });
   }
