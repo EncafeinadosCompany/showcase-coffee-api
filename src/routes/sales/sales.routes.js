dfedc3a599bc32cbdec37b3fb45c8f77fabd166e
Dm29015 <dmcorrea670@soy.sena.edu.co>
2025-02-04 12:28:01 -0500

    perf: :zap: Improved performance when creating a sale
    Improved service logic

--- src/routes/sales/sales.routes.js
+++ src/routes/sales/sales.routes.js
@@ -1,9 +1,10 @@
+const { sequelize } = require('../../config/connection');
+
+const SalesService = require('../../services/sales/sales.service');
 const SalesController  = require('../../controllers/sales/sales.controller');
 const SalesRepository = require('../../repositories/sales/sales.repository');
-const SalesService = require('../../services/sales/sales.service');
 const SalesVariantRepository = require('../../repositories/sales/salesVariant.repository');
 const ProductVariantsRepository = require('../../repositories/products/products.repository');
-const { sequelize } = require('../../config/connection');
 
 const saleVariantRepository = new SalesVariantRepository();
 const productVariantsRepository = new ProductVariantsRepository();
@@ -16,9 +17,7 @@ const router = require('express').Router();
 
 router
     .get('/', (req, res) => saleController.getAllSales(req, res))
-    .get('/:id', (req, res) => saleController.getSalesById(req, res))
-    .post('/', (req, res) => saleController.createSaleWithDetails(req, res))
-    .get('/variant/all', (req, res) => saleController.getAllSaleVariant(req, res))
-    .get('/variant/:id', (req, res) => saleController.getSaleVariantById(req, res))
+    .get('/:id', (req, res) => saleController.getSaleById(req, res))
+    .post('/', (req, res) => saleController.createSale(req, res))
 
 module.exports = router ;
