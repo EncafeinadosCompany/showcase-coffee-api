1cffdb9493388afae52f0ebd40aab07b8acf0816
Dm29015 <dmcorrea670@soy.sena.edu.co>
2025-02-10 12:35:04 -0500

    feat: :sparkles: Add user validation middleware

--- src/middlewares/validateRequest.js
+++ src/middlewares/validateRequest.js
@@ -1,27 +1,27 @@
 const { validationResult } = require("express-validator");
 
-const validationMiddleware = (req, res, next) => {
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const translatedErrors = errors.array().map((error) => ({
-      field: error.path,
-      msg: errorMessages[lang]?.[error.msg] || error.msg,
-    }));
-
-    return res.status(400).json({ errors: translatedErrors });
-  }
-
-  next();
-};
-
 // const validationMiddleware = (req, res, next) => {
 
 //   const errors = validationResult(req);
-//   if (!errors.isEmpty()) { 
-//       return res.status(400).json({ errors: errors.array() });
+//   if (!errors.isEmpty()) {
+//     const translatedErrors = errors.array().map((error) => ({
+//       field: error.path,
+//       msg: errorMessages[lang]?.[error.msg] || error.msg,
+//     }));
+
+//     return res.status(400).json({ errors: translatedErrors });
 //   }
+
 //   next();
 // };
+
+const validationMiddleware = (req, res, next) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) { 
+      return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
 
 module.exports = validationMiddleware;
