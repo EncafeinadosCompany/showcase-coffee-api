f6ebabfb3d676886b9fea08f251fe19f658c4f7d
Crony <lopezcrony@gmail.com>
2025-02-11 13:36:50 -0500

    changes in ShoppingSeeder

--- src/db/seeders/20250210203928-shopping.js
+++ src/db/seeders/20250210203928-shopping.js
@@ -5,7 +5,7 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     const STORE_ID = 1; 
     const EMPLOYEE_ID = 2; 
-    const VARIANT_PRODUCT_IDS = [1]; 
+    const VARIANT_PRODUCT_IDS = [1, 2]; 
 
     const shoppingData = {
       id_store: STORE_ID,
@@ -22,13 +22,13 @@ module.exports = {
 
     const shoppingId = shoppingResult.id;
 
-    const shoppingVariantData = VARIANT_PRODUCT_IDS.map((id_variant_products) => ({
+    const shoppingVariantData = VARIANT_PRODUCT_IDS.map((id_variant_products, index) => ({
       id_shopping: shoppingId,
       id_variant_products,
       roasting_date: new Date(),
-      quantity: 5,
-      shopping_price: 10000, 
-      sale_price: 15000, 
+      quantity: index === 0 ? 10 : 5,
+      shopping_price: index === 0 ? 15.50 : 10.00,
+      sale_price: index === 0 ? 20.00 : 15.00, 
       status: true,
       created_at: new Date(),
       updated_at: new Date(),
